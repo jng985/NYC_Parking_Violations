972df224f27bb777c76663792178c2182fc64486
Jonathan Ng <jng20@binghamton.edu>
2020-02-22 07:03:00 -0500

    argparse

--- part1/main.py
+++ part1/main.py
@@ -2,19 +2,26 @@ import os
 import sys
 import json
 import pprint
-from src.bigdata1.api import get_results
 import pandas as pd
 import numpy as np
+import argparse
 from functools import reduce
 
+from src.bigdata1.api import get_results, output_results
+
 if __name__ == "__main__":
-    kwargs  = {}
-    for arg in sys.argv[1:]:
-        key, value = arg.split('=')
-        kwargs[key] = value 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--page_size", type=int)
+    parser.add_argument("--num_pages", default=4, type=int)
+    parser.add_argument("--output", default=None)
+    args = parser.parse_args()
+    print(args)
+
+    response = get_results(args.page_size, args.num_pages)
+    if args.output:
+        output_results(response, args.output)
+    else:
+        pprint.pprint(response, indent=4)
+
+
     
-    print(kwargs)
-    page_size = kwargs['--page_size']
-    response = get_results(page_size)
-    
-    #pprint.pprint(response)
--- part1/src/bigdata1/api.py
+++ part1/src/bigdata1/api.py
@@ -10,17 +10,16 @@ import numpy as np
 data_id = 'nc67-uf89'
 client = Socrata('data.cityofnewyork.us', os.environ.get("APP_KEY"))
 
-def get_results(page_size, num_pages=4, output=None):
+def get_results(page_size, num_pages):
     pages = {}
     for page in range(num_pages):
         offset = page * page_size
         page_response = client.get(data_id, limit=page_size, offset=offset)
         pages[page] = page_response
-        print(pd.DataFrame(page_response))
     return pages
 
-def output_results(pages):
-    with open('results.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+def output_results(results, output):
+    with open(output, 'w', encoding='utf-8') as f:
+        json.dump(results, f, ensure_ascii=False, indent=4)
 
 

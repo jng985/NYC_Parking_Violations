7a5ada0315956b5a72cf5b05890fb9f57163bb14
Jonathan Ng <jng20@binghamton.edu>
2020-03-08 07:21:01 -0400

    imgs p3

--- part2/src/bigdata1/api.py
+++ part2/src/bigdata1/api.py
@@ -1,6 +1,7 @@
 import os
 import json 
 import pprint
+from time import sleep
 from sodapy import Socrata
 from src.bigdata1.elastic import create_and_update_index, push_record
 
@@ -17,14 +18,24 @@ def get_results(page_size, num_pages, output, push_elastic):
         es = create_and_update_index('bigdata1', 'violations')
     for page in range(num_pages):
         offset = page * page_size
-        page_records = client.get(data_id, limit=page_size, offset=offset)
+        try:
+            page_records = client.get(data_id, limit=page_size, offset=offset)
+        except:
+            sleep(10)
+            page_records = client.get(data_id, limit=page_size, offset=offset)
         for record in page_records:
             if output:
                 add_record(record, output)
             else:
                 pprint.pprint(record, indent=4)
             if push_elastic:
-                push_record(record, es, 'bigdata1', 'violations')
+                try:
+                    push_record(record, es, 'bigdata1', 'violations')
+                except:
+                    sleep(10)
+                    push_record(record, es, 'bigdata1', 'violations')
+
+
 
 def create_records(output):
     with open(output, 'w') as out_file:
